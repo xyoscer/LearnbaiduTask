daa6626803233f58882bf7319d5f779e5bea7282
郗银 <1498546245@qq.com>
2016-07-03 20:54:21 +0800

    Create task16.js
    使用js对表单进行了简单验证,使用innerHTML向页面内插入内容，使用给每一个行的单击按钮添加删除事件，达到删除效果，还没有使用到事件绑定机制

---
+++ task16.js
@@ -0,0 +1,82 @@
+/**
+ * aqiData，存储用户输入的空气指数数据
+ * 示例格式：
+ * aqiData = {
+ *    "北京": 90,
+ *    "上海": 40
+ * };
+ */
+var aqiData = {};
+
+/**
+ * 从用户输入中获取数据，向aqiData中增加一条数据
+ * 然后渲染aqi-list列表，增加新增的数据
+ */
+function addAqiData() {
+	var cityInput = document.getElementById('aqi-city-input');
+	var num = document.getElementById('aqi-value-input');
+    var cityInputValue =cityInput.value.trim();
+    var  numINputValue =num.value.trim();
+  
+    	if(!cityInputValue.match(/^[A-Za-z\u4E00-\u9FA5]+$/)){
+    	 alert("城市名字必须为中英文字符");
+    	 cityInput.focus();
+    	 return;
+    	}
+      if(!numINputValue.match(/^[0-9]+$/)){
+    	alert("请输入整数！")
+    	num.focus();
+    	return;
+    }
+    return aqiData[cityInputValue] =numINputValue;
+}
+
+/**
+ * 渲染aqi-table表格
+ */
+function renderAqiList() {
+   var table = document.getElementById('aqi-table');
+   var tableItem = "<tr>"+"<td>"+"城市"+"</td>"+"<td>"+"空气质量"+"</td>"+"<td>"+"操作"+"</td>";
+   	for(var pro in aqiData){
+
+   	tableItem+="<tr>"+"<td>"+pro+"</td>"+"<td>"+aqiData[pro]+"</td>"+"<td>"+"<button onclick='delBtnHandle(\""+pro+"\")'>"+"删除"+"</button>"+"</td>";
+   }
+
+   if(pro){
+   	table.innerHTML = tableItem;
+   }else{
+   	  table.innerHTML = "<p>"+"没有内容显示"+"</p>";
+   }
+}
+
+/**
+ * 点击add-btn时的处理逻辑
+ * 获取用户输入，更新数据，并进行页面呈现的更新
+ */
+function addBtnHandle() {
+  addAqiData();
+  renderAqiList();
+}
+
+/**
+ * 点击各个删除按钮的时候的处理逻辑
+ * 获取哪个城市数据被删，删除数据，更新表格显示
+ */
+function delBtnHandle(cityInputValue) {
+ 
+  delete aqiData[cityInputValue] ;
+  renderAqiList();
+}
+
+function init() {
+
+  // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
+var add_btn = document.getElementById('add-btn');
+
+    add_btn.addEventListener("click",addBtnHandle,false);
+  // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+   
+}
+window.onload = function(){
+	init();
+}
